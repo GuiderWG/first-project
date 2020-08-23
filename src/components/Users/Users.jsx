9c73bce0e0d1198a694c839b0a4c12842262b0f2
Vladimir <guiderwg@gmail.com>
2020-08-23 23:24:28 +0400

    setUsers

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,18 +1,61 @@
 import React from "react";
 import s from "./Users.module.css"
 
-let Users = (props) => {
+let Users = ({setUsers, users, unfollow, follow}) => {
+
+  if (users.length === 0) {
+    setUsers([
+      {
+        id: 1,
+        followed: false,
+        fullName: 'Dmitriy',
+        status: 'I\'m okay',
+        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
+        location: {city: 'Minsk', country: 'Belarus'}
+      },
+      {
+        id: 2,
+        followed: false,
+        fullName: 'Vladimir',
+        status: 'The day is cold now',
+        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
+        location: {city: 'Samara', country: 'Russia'}
+      },
+      {
+        id: 3,
+        followed: true,
+        fullName: 'Rail',
+        status: 'I hope that everything alright',
+        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
+        location: {city: 'Kiev', country: 'Ukraine'}
+      },
+      {
+        id: 4,
+        followed: false,
+        fullName: 'Jane',
+        status: 'it looks very strange',
+        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
+        location: {city: 'Paris', country: 'France'}
+      },
+    ])
+  }
+
+
   return (
       <div className={s.users}>
         {
-          props.users.map(user =>  <div className={s.item} key={user.id}>
+          users.map(user => <div className={s.item} key={user.id}>
             <div className={s.leftCol}>
               <div className={s.photo}>
                 <img src={user.imgUrl} alt=""/>
               </div>
               {user.followed
-                  ? <button className={`${s.button} ${s.button_gray}`} onClick={() => { props.unfollow(user.id) } }>Unfollow</button>
-                  : <button className={`${s.button} ${s.button_red}`} onClick={() => { props.follow(user.id) }}>Follow</button>}
+                  ? <button
+                      className={`${s.button} ${s.button_gray}`}
+                      onClick={() => {unfollow(user.id)}}>Unfollow</button>
+                  : <button
+                      className={`${s.button} ${s.button_red}`}
+                      onClick={() => {follow(user.id)}}>Follow</button>}
             </div>
             <div className={s.rightCol}>
               <div className={s.info}>
