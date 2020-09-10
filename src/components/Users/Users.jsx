5c0daa2399d75c7a3feb1ef0d2d0f6733339cd65
Vladimir <guiderwg@gmail.com>
2020-09-10 10:24:13 +0400

    add axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,53 +1,25 @@
 import React from "react";
 import s from "./Users.module.css"
+import * as axios from "axios"
+
 
 let Users = ({setUsers, users, unfollow, follow}) => {
 
   if (users.length === 0) {
-    setUsers([
-      {
-        id: 1,
-        followed: false,
-        fullName: 'Dmitriy',
-        status: 'I\'m okay',
-        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
-        location: {city: 'Minsk', country: 'Belarus'}
-      },
-      {
-        id: 2,
-        followed: false,
-        fullName: 'Vladimir',
-        status: 'The day is cold now',
-        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
-        location: {city: 'Samara', country: 'Russia'}
-      },
-      {
-        id: 3,
-        followed: true,
-        fullName: 'Rail',
-        status: 'I hope that everything alright',
-        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
-        location: {city: 'Kiev', country: 'Ukraine'}
-      },
-      {
-        id: 4,
-        followed: false,
-        fullName: 'Jane',
-        status: 'it looks very strange',
-        imgUrl: 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1',
-        location: {city: 'Paris', country: 'France'}
-      },
-    ])
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      setUsers(response.data.items);
+    });
+
   }
 
-
+  console.log(users)
   return (
       <div className={s.users}>
         {
           users.map(user => <div className={s.item} key={user.id}>
             <div className={s.leftCol}>
               <div className={s.photo}>
-                <img src={user.imgUrl} alt=""/>
+                <img src={user.photos.small ? user.photos.small : 'https://i.stack.imgur.com/T17Mh.png?s=328&g=1'} alt=""/>
               </div>
               {user.followed
                   ? <button
@@ -60,17 +32,17 @@ let Users = ({setUsers, users, unfollow, follow}) => {
             <div className={s.rightCol}>
               <div className={s.info}>
                 <div className={s.fullName}>
-                  {user.fullName}
+                  {user.name}
                 </div>
                 <span className={s.status}>{user.status}</span>
               </div>
 
               <div className={s.location}>
                 <div className={s.country}>
-                  {user.location.country}
+                  {'user.location.country'}
                 </div>
                 <div className={s.city}>
-                  {user.location.city}
+                  {'user.location.city'}
                 </div>
               </div>
             </div>
