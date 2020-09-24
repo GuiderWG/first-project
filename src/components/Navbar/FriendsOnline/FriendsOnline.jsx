b79bff96ed18b57c27801b8fa3a7af9a2333cf4e
Vladimir <guiderwg@gmail.com>
2020-09-24 17:24:22 +0400

    FR

--- src/components/Navbar/FriendsOnline/FriendsOnline.jsx
+++ src/components/Navbar/FriendsOnline/FriendsOnline.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 import s from './FriendsOnline.module.css';
 import FriendsItem from "./FriendsItem/FriendsItem";
-import * as axios from "axios"
 
+let FriendsOnline = (props) => {
+  let friendItems = props.friends.map(friend => {
+    return <FriendsItem id={friend.id} name={friend.name} img={friend.photos.small} key={friend.id}/>
+  });
 
-class FriendsOnline extends React.Component {
-  componentDidMount() {
-    axios.get('https://social-network.samuraijs.com/api/1.0/users?count=4').then(response => {
-      if (this.props.friends.length === 0) {
-        this.props.setFriend(response.data.items);
-      }
-    });
-  }
-
-  render() {
-    let friendItems = this.props.friends.map(friend => {
-      return <FriendsItem id={friend.id} name={friend.name} img={friend.photos.small} key={friend.id}/>
-    });
-    return (
-        <div className={s.friends}>
-          <div className={s.title}>Friends online</div>
-          <div className={s.friendsGroup}>
-            {friendItems}
-          </div>
+  return (
+      <div className={s.friends}>
+        <div className={s.title}>Friends online</div>
+        <div className={s.friendsGroup}>
+          {friendItems}
         </div>
-    );
-  }
+      </div>
+  )
 }
 
 export default FriendsOnline;
--- src/components/Navbar/FriendsOnline/FriendsOnlineContainer.jsx
+++ src/components/Navbar/FriendsOnline/FriendsOnlineContainer.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import {connect} from "react-redux";
 import FriendsOnline from "./FriendsOnline";
 import {setFriendsOnlineAC} from "../../../redux/navbarReducer";
+import * as axios from "axios";
+
+class FriendsOnlineContainer extends React.Component {
+  componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users?count=4').then(response => {
+      if (this.props.friends.length === 0) {
+        this.props.setFriend(response.data.items);
+      }
+    });
+  }
+
+  render() {
+    return <FriendsOnline friends={this.props.friends} />
+  }
+}
 
 let mapStateToProps = (state) => {
   return {
     friends: state.navbar.friends
   }
-};
+}
 
 let mapDispatchToProps = (dispatch) => {
   return {
@@ -15,9 +30,6 @@ let mapDispatchToProps = (dispatch) => {
       dispatch(setFriendsOnlineAC(friends));
     }
   }
+}
 
-};
-
-const FriendsOnlineContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsOnline);
-
-export default FriendsOnlineContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(FriendsOnlineContainer);
