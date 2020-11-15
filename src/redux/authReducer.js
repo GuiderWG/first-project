dc3019516ca66b3d3cd7bc8af71f3829bcf9cd7e
Vladimir <guiderwg@gmail.com>
2020-11-16 01:00:17 +0400

    get

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,47 +1,43 @@
-import {authAPI} from '../api/api';
+import { authAPI } from '../api/api';
 
 const SET_USER_DATA = 'SET_USER_DATA';
 const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
-
-let initialState = {
+const initialState = {
   id: null,
   email: null,
   login: null,
   isAuth: false,
-  isFetching: false
+  isFetching: false,
 };
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USER_DATA:
-      return  {
+      return {
         ...state,
         ...action.data,
         isAuth: true,
-      }
+      };
     case TOGGLE_IS_FETCHING:
-      return  { ...state, isFetching: action.isFetching }
+      return { ...state, isFetching: action.isFetching };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
-export const setAuthUserData = (id, email, login) => ({type: SET_USER_DATA, data: {id, email, login}});
+export const toggleIsFetching = (isFetching) => ({ type: TOGGLE_IS_FETCHING, isFetching });
+export const setAuthUserData = (id, email, login) => ({ type: SET_USER_DATA, data: { id, email, login } });
 export const getAuthUserData = () => (dispatch) => {
   dispatch(toggleIsFetching(true));
-  authAPI
-    .getMyProfile()
-    .then(response => {
-      if (response.data.resultCode === 0) {
-        let {id, email, login} = response.data.data;
-        dispatch(setAuthUserData(id, email, login));
-      }
+  authAPI.getMyProfile().then((response) => {
+    if (response.data.resultCode === 0) {
+      const { id, email, login } = response.data.data;
+      dispatch(setAuthUserData(id, email, login));
+    }
 
-      dispatch(toggleIsFetching(false));
-    });
-}
-
+    dispatch(toggleIsFetching(false));
+  });
+};
 
 export default authReducer;
