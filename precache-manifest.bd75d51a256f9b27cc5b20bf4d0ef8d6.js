aa2ab22c2125727c8aa83a8feefce8f8df31af68
Vladimir <guiderwg@gmail.com>
2021-01-26 18:55:21 +0400

    Updates

---
+++ precache-manifest.bd75d51a256f9b27cc5b20bf4d0ef8d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6addc3fca9b9c519beb798e0eb96ba8",
+    "url": "/first-project/index.html"
+  },
+  {
+    "revision": "0b23b93c565a6579ea21",
+    "url": "/first-project/static/css/3.a8a8f818.chunk.css"
+  },
+  {
+    "revision": "f6edf588507b5b3ef898",
+    "url": "/first-project/static/css/4.c5601370.chunk.css"
+  },
+  {
+    "revision": "557b91d27112d4a29cb6",
+    "url": "/first-project/static/css/main.ce28230d.chunk.css"
+  },
+  {
+    "revision": "850503d140c5c43a3b3e",
+    "url": "/first-project/static/js/2.b4aeb0b2.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/first-project/static/js/2.b4aeb0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b23b93c565a6579ea21",
+    "url": "/first-project/static/js/3.5999587b.chunk.js"
+  },
+  {
+    "revision": "f6edf588507b5b3ef898",
+    "url": "/first-project/static/js/4.b9455856.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/first-project/static/js/4.b9455856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557b91d27112d4a29cb6",
+    "url": "/first-project/static/js/main.59ca7267.chunk.js"
+  },
+  {
+    "revision": "440b890fe1910774dfc1",
+    "url": "/first-project/static/js/runtime-main.f4d87502.js"
+  },
+  {
+    "revision": "ddd11c2b829e68ee21b9030bf3cc2727",
+    "url": "/first-project/static/media/reactjs-ar21.ddd11c2b.svg"
+  }
+]);
--- src/components/Dialogs/MessageItem/MessageItem.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import s from '../Dialogs.module.css';
-
-const MessageItem = (props) => <div className={s.message}>{props.message}</div>;
-
-export default MessageItem;
